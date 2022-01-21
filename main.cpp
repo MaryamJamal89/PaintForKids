8d081a04029d523610c085a13a0e9a8e8992ff73
Maryam El-Aiat <maryamalayaat@gmail.com>
2022-01-21 22:26:12 +0200

    commented lines back

--- main.cpp
+++ main.cpp
@@ -3,6 +3,13 @@
 
 int main()
 {
+	/*
+	GUI* g=new GUI();
+	
+	window* w=g->CreateWind(200, 300, 10, 10);
+	int x, y;
+	w->WaitMouseClick(x, y);
+	*/
 	
 	ApplicationManager AppManager;
 	AppManager.Run();	
