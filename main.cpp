7493645633316dd93e8ebd1c6fd0636395fa3034
Maryam El-Aiat <maryamalayaat@gmail.com>
2022-01-21 22:23:59 +0200

    removed commented lines

--- main.cpp
+++ main.cpp
@@ -3,13 +3,6 @@
 
 int main()
 {
-	/*
-	GUI* g=new GUI();
-	
-	window* w=g->CreateWind(200, 300, 10, 10);
-	int x, y;
-	w->WaitMouseClick(x, y);
-	*/
 	
 	ApplicationManager AppManager;
 	AppManager.Run();	
