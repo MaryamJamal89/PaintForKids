0bec767b589b7cd390311015222e0d6425bff128
RandaHadad <randahada99@gmail.com>
2022-01-29 00:03:51 +0200

    solve the problem of change select coloring

--- Actions/ActionChangeColor.cpp
+++ Actions/ActionChangeColor.cpp
@@ -1,9 +1,9 @@
 #include "ActionChangeColor.h"
 #include "../ApplicationManager.h"
 #include "../GUI/GUI.h"
+#include"../Figures/CFigure.h"
 
-
-ActionChangeColor::ActionChangeColor(ApplicationManager* pApp, color c, int drawORfill) : Action(pApp), colour(c), DrawOrFill(drawORfill)
+ActionChangeColor::ActionChangeColor(ApplicationManager* pApp, color c, int drawORfill, CFigure* figure) : Action(pApp), colour(c), DrawOrFill(drawORfill),Figure(figure)
 {
 }
 
@@ -12,13 +12,21 @@ void ActionChangeColor::Execute() {
 	
 	// Check If The Mode if Change Drawing Color
 	if (DrawOrFill == 1) {
-		UI.DrawColor = colour;
+		if (Figure != NULL)
+			Figure->ChngDrawClr(colour);
+		else
+			UI.DrawColor = colour;
+
 		pManager->UpdateInterface();
 	}
 	// Check If The Mode if Change Filling Color
 	else if (DrawOrFill == 2) {
-		UI.isFilled = true;
-		UI.FillColor = colour;
+		if (Figure != NULL)
+			Figure->ChngFillClr(colour);
+		else {
+			UI.isFilled = true;
+			UI.FillColor = colour;
+		}
 		pManager->UpdateInterface();
 	}
 	// Check If The Mode if Change background Color
